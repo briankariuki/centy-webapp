d7fff90ca6e2a15f096e0582248692b341243e21
briankariuki <bryankariuki79@gmail.com>
2020-05-11 13:27:12 +0300

    Post CSS Fix v3

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
   content: [
     "./src/**/*.html",
-    "./src/**/*.vue",
+    "./src/**/*.js",
     "./src/**/*.jsx",
     // etc.
   ],

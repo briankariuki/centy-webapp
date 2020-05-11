46ea27e70431293068e67498e76b490dae29fb6d
briankariuki <bryankariuki79@gmail.com>
2020-05-11 13:01:38 +0300

    Change Purge CSS Options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
 module.exports = {
   purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      './src/**/*.html',
-      './src/**/*.js',
-      './src/**/*.jsx',
-    ],
-    options: {
-
-    },
+    // enabled: process.env.NODE_ENV === 'production',
+    content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.jsx"],
+    options: {},
   },
   theme: {
     extend: {},
   },
   variants: {},
   plugins: [],
-}
+};

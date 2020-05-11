98374e34347a33a583898bfc41509bce1d69fdca
briankariuki <bryankariuki79@gmail.com>
2020-05-11 14:18:44 +0300

    final fix test 2

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,5 @@
 const tailwindcss = require("tailwindcss");
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  // Specify the paths to all of the template files in your project
-  content: [
-    "./src/**/*.html",
-    "./src/**/*.js",
-    "./src/**/*.jsx",
-    // etc.
-  ],
 
-  // This is the function used to extract class names from your templates
-  defaultExtractor: (content) => {
-    // Capture as liberally as possible, including things like `h-(screen-1.5)`
-    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-
-    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-
-    return broadMatches.concat(innerMatches);
-  },
-});
 module.exports = {
-  plugins: [
-    tailwindcss("./tailwind.config.js"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-  ],
+  plugins: [tailwindcss("./tailwind.config.js"), require("autoprefixer")],
 };

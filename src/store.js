a7c5a86791c1132b0ecb3f6da82299af7bc6bf2b
briankariuki <bryankariuki79@gmail.com>
2020-05-11 20:07:40 +0300

    changed devtools

--- src/store.js
+++ src/store.js
@@ -2,20 +2,18 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-
 const initialState = {};
 
 const middleware = [thunk];
 
 //Create store
 const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware)
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
-
 
 export default store;
